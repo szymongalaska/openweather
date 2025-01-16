99b9eb335bfbc7826b20f624749c43ed74a23e77
Szymon Gałąska <siubidubasiabadabba@gmail.com>
2025-01-16 19:56:31 +0100

    created config file

---
+++ src/config/openweather.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'api_key' => getenv('OPENWEATHER_API_KEY') ?: null,
+    'api_version' => '2.5',
+    'geo_api_version' => '1.0',
+    'language' => getenv('OPENWEATHER_LANGUAGE') ?? 'en',
+    'date_format' => 'd/m/Y',
+    'time_format' => 'h:i A',
+    'day_format' => 'l',
+    'temperature' => 'c',
+    'url' => 'https://api.openweathermap.org/',
+];
