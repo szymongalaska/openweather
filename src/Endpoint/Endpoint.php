c785a7c6397921c68ac069b9e5333eb326583b0d
Szymon Gałąska <siubidubasiabadabba@gmail.com>
2025-01-17 11:05:22 +0100

    created base class for endpoints

---
+++ src/Endpoint/Endpoint.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bejblade\OpenWeather\Endpoint;
+
+use Bejblade\OpenWeather\Interface\EndpointInterface;
+use Bejblade\OpenWeather\OpenWeatherClient;
+
+/**
+ * Endpoint class which calls API endpoint
+ */
+abstract class Endpoint implements EndpointInterface
+{
+    /**
+     * API version to use in endpoint request
+     * @var string
+     */
+    protected string $api_version;
+
+    /**
+     * Client used to call API
+     * @var OpenWeatherClient
+     */
+    protected OpenWeatherClient $client;
+
+    public function __construct(OpenWeatherClient $client, string $api_version)
+    {
+        $this->client = $client;
+        $this->api_version = $api_version;
+    }
+
+    /**
+     * Return response from API as array
+     * @param array $options Options to use in API request
+     * @return array
+     */
+    protected function getResponse(array $options = []): array
+    {
+        $this->validate($options);
+        return $this->client->callApi($this->buildUrl(), ['query' => $options]);
+    }
+
+    abstract public function call(array $options = []);
+
+    abstract public function getEndpoint(): string;
+
+    /**
+     * Build URL which will be used to make API request
+     * @return string
+     */
+    abstract protected function buildUrl(): string;
+
+    /**
+     * Returns array of available options to use in request
+     * @return array
+     */
+    abstract protected function getAvailableOptions(): array;
+
+    /**
+     * Validate options provided are supported or are correctly set
+     * @param array $options
+     * @return void
+     */
+    protected function validate(array $options): void
+    {
+        $this->validateOptionsSupport($options);
+    }
+
+    /**
+     * Validate that options provided are supported by current endpoint
+     * @param array $options
+     * @throws \InvalidArgumentException
+     * @return bool
+     */
+    protected function validateOptionsSupport(array $options): bool
+    {
+        $invalidOptions = array_diff(array_keys($options), $this->getAvailableOptions());
+        if (!empty($invalidOptions)) {
+            throw new \InvalidArgumentException('Options not supported.');
+        }
+
+        return true;
+    }
+}
