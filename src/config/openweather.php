cf9e414be7eac42cd917f50cb8899b124b859389
Szymon Gałąska <siubidubasiabadabba@gmail.com>
2025-01-17 11:17:40 +0100

    fixed bug in language

--- src/config/openweather.php
+++ src/config/openweather.php
@@ -4,7 +4,7 @@ return [
     'api_key' => getenv('OPENWEATHER_API_KEY') ?: null,
     'api_version' => '2.5',
     'geo_api_version' => '1.0',
-    'language' => getenv('OPENWEATHER_LANGUAGE') ?? 'en',
+    'language' => getenv('OPENWEATHER_LANGUAGE') ?: 'en',
     'date_format' => 'd/m/Y',
     'time_format' => 'h:i A',
     'day_format' => 'l',
