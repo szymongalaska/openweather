90a79ef3704d59b965c074a181355a762bdd5dcd
Szymon Gałąska <siubidubasiabadabba@gmail.com>
2025-01-17 10:25:19 +0100

    Created configuration expection

---
+++ src/Exception/InvalidConfigurationException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Bejblade\OpenWeather\Exception;
+
+use Exception;
+
+/**
+ * Exception class for configuration
+ */
+class InvalidConfigurationException extends Exception
+{
+    public function __construct(string $message = "", int $code = 0, \Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
