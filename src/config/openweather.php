2b1e5a74adeaedaaf5005efeedcbb6308a513eeb
Szymon Gałąska <siubidubasiabadabba@gmail.com>
2025-01-22 14:32:56 +0100

    added one_call_api_version configuration

--- src/config/openweather.php
+++ src/config/openweather.php
@@ -4,6 +4,7 @@ return [
     'api_key' => getenv('OPENWEATHER_API_KEY') ?: null,
     'api_version' => '2.5',
     'geo_api_version' => '1.0',
+    'one_call_api_version' => '3.0',
     'language' => getenv('OPENWEATHER_LANGUAGE') ?: 'en',
     'date_format' => 'd/m/Y',
     'time_format' => 'H:i',
